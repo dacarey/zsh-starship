c2baf4855c14cb47adaed80e99eb4d55f2e0eb68
Dave Carey <dacarey@gmail.com>
2025-01-02 17:51:33 +0000

    chore: improve ZAP PLUGIN loading with enhanced error messages and configuration handling

--- zsh-starship.plugin.zsh
+++ zsh-starship.plugin.zsh
@@ -3,22 +3,29 @@
 # Default logging mechanism (fallback to echo)
 log_message=${log_message:-echo}
 
-# Starship prompt configuration
-STARSHIP_CONFIG_DIR="$HOME/.config/starship"
-STARSHIP_CONFIG_FILE="$STARSHIP_CONFIG_DIR/starship.toml"
+$log_message "==> ZAP PLUGIN loading - zsh-starship"
 
-# Check for Starship configuration
-if [[ ! -f $STARSHIP_CONFIG_FILE ]]; then
-    $log_message "Error: Starship configuration file not found in $STARSHIP_CONFIG_DIR."
+# Exit if the 'starship' command can not be found
+if ! (( $+commands[starship] )); then
+    echo "ERROR: 'starship' command not found. ZAP plugin 'zsh-starship' will not be loaded."
     return
 fi
 
-$log_message "==> ZAP PLUGIN loading - zsh-starship"
-
-# Configure Starship if executable is available
-if command -v starship >/dev/null 2>&1; then
-    eval "$(starship init zsh)"
-    $log_message "    -- STARSHIP is configured to use '$STARSHIP_CONFIG_FILE'."
-else
-    $log_message "Error: Starship executable not found in PATH."
+# If the 'STARSHIP_CONFIG' environment variable is not populated then change
+# the default theme configuration to the one provided in this plugin
+if [[ -z "$STARSHIP_CONFIG" ]]; then
+    export STARSHIP_CONFIG="${0:A:h}/theme/starship.toml"
 fi
+
+$log_message "    -- STARSHIP is configured to use '$STARSHIP_CONFIG'."
+
+# Init cache directory for `starship` command
+local INIT_CACHE_DIR="${0:A:h}/init"
+
+# Only regenerate init script if older than 24 hours, or does not exist
+if [[ ! -f "$INIT_CACHE_DIR/_starship"  ||  ! $(find "$INIT_CACHE_DIR/_starship" -newermt "24 hours ago" -print) ]]; then
+    starship init zsh --print-full-init >| "$INIT_CACHE_DIR/_starship"
+fi
+
+# Initialise the Starship Prompt
+source "$INIT_CACHE_DIR/_starship"
